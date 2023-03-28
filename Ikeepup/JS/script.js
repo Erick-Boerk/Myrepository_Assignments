2fa61978a7d780adf6d6295fc7256837e71f5b17
ERICK BOERK <122455808+Erick-Boerk@users.noreply.github.com>
2023-03-28 15:51:47 +0200

    Responsive Grid
    Assignment for week2

---
+++ Ikeepup/JS/script.js
@@ -0,0 +1,8 @@
+var msg="Hello world! I am now ready to walk onto the earth with honorable aspect"
+		console.log("My inspiration after having thinking for a while :\n"+msg);
+
+      var x = 5;
+      x *= 10/2;
+      var y = x;
+      
+      console.log(y);
